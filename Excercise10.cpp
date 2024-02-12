77cdb64947df4f4ac653b82e53f2cb2e94ac74bd
MarwanMo11 <MarwanMo11@aucegypt.edu>
2024-02-12 22:08:57 +0200

    updated file positions

--- Excercise10.cpp
+++ Excercise10.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+    int  sum = 0;
+    int max = -999999999;
+    int nums[] = {2,93,-5,-4,6,7};
+    int n = 6;
+    for (int i = 0; i < n; i++){
+        sum = 0;
+        for (int j = i+1; j < n; j++){
+                sum = sum + nums[j];
+                if (sum > max){
+                    max = sum;
+                }
+
+
+            }
+        }
+
+    cout << max;
+    }
+
+
+
