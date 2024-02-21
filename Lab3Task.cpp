5c89e4d375d227c221b12996bc5f128fa5ff724e
MarwanMo11 <MarwanMo11@aucegypt.edu>
2024-02-21 20:48:04 +0200

    updated file positions

--- Lab3Task.cpp
+++ Lab3Task.cpp
@@ -12,7 +12,7 @@ struct Jobs{
     int deadline;
     int profit;
 
-    bool operator<(const Jobs& other) const{
+    bool operator<(const Jobs& other) const{ // overloading the comparison operator for the sort
         return profit > other.profit;
     }
 };
@@ -21,12 +21,12 @@ struct Jobs{
 
 
 void maxprofit(int maxdeadline, vector<Jobs> arr[]){
-    sort(arr->begin(), arr->end());
+    sort(arr->begin(), arr->end()); // sorting the vector based on the profit value (Pre sorting)
     int day = 1;
 
-    for(const auto& Jobs: *arr){
-        while (day<=maxdeadline){
-        if (day <= Jobs.deadline && day <= Jobs.deadline){
+    for(const auto& Jobs: *arr){ // going over the whole vector
+        while (day<=maxdeadline){ // while we did not reach the max deadline keep the code running
+        if (day <= Jobs.deadline && day <= Jobs.deadline){ // if the deadline of specific job fits criteria pick it based on value
             cout << "The job " << Jobs.id  << " is done before day " << day << " for a profit of " << Jobs.profit << endl;
             day++;
             break;
