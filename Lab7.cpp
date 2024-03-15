d3cddb8938ddc5376815284208817a8241001dd1
MarwanMo11 <MarwanMo11@aucegypt.edu>
2024-03-15 23:24:26 +0200

    added comments and complexity and organized code

--- Lab7.cpp
+++ Lab7.cpp
@@ -8,7 +8,90 @@
 
 using namespace std;
 
+// Part 1
 
+struct bucket{
+    int id;
+    bool full;
+
+};
+
+// requires the array to be sorted
+/*
+ T(n) where n is the size of the array is equal to 2T(n/2) + 2 where the constant is simple operations and the 2T(n/2) is
+ what is returned recursively. A pattern is formed and when computed we get logn complexity O(logn) this is ofcourse assuming
+ the array given is sorted
+
+ */
+int storage(bucket arr[],int start, int end){
+    if(arr[end].full == 0){ // base case  means all empty
+        return 0;
+    }
+    if(arr[start].full == 1){ // base case means all full
+        return (end+1-start);
+
+    } else if (arr[start].full == 0){ // array needs more splitting to be done to determine
+        return storage(arr, start, start+(end-start)/2) + storage(arr, (start+(end-start)/2)+1, end);} else {
+
+
+
+    }
+
+
+
+}
+
+// Part 2
+/*
+ Calculating the T(n) of the divide and conquer approach where n is the size of the array we get 2T(n/2)+n and when
+ simplified we get the complexity of O(nlogn) this applies for both functions.
+
+ */
+int maxhelper(int arr[], int s, int e) {
+    // initialization takes constant time
+    int sum = 0;
+    int left = INT_MIN;
+    int right = INT_MIN; // Fix: Initialize right to INT_MIN instead of INT_MAX
+    int middle = (s + e) / 2;
+
+    // each for loop takes n/2 so n/2 + n/2 = n so O(n)
+    for (int i = middle + 1; i <= e; i++) { // Fix: Change the loop condition to <= instead of >=
+        sum += arr[i];
+        right = std::max(right, sum); // Fix: Change left to right
+    }
+
+    sum = 0; // Reset sum for the next loop
+
+    for (int i = middle; i >= s; i--) {
+        sum += arr[i];
+        left = std::max(left, sum); // Fix: Change right to left
+    }
+
+    return left + right;
+}
+
+int max(int arr[], int s, int e) {
+    if (s == e) {
+        return arr[s];
+    }
+
+    int middle = (s + e) / 2;
+    int maxleft = max(arr, s, middle);
+    int maxright = max(arr, middle + 1, e);
+    int maxoverlap = maxhelper(arr, s, e);
+
+    return std::max({maxleft, maxright, maxoverlap}); // Fix: Use std::max to find the maximum of three values
+}
+
+
+// Part 3
+
+/*
+ The functions dist and stripClosest and closestUtil etc... have the lowest complexity and the most dominant function is
+ closestPair which also is done with divide and conquer approach. We have for that function T(n) where n is the number of points
+ equal to nlogn + 2T(n/2) and when calculated we have a time complexity of O(nlogn)
+
+ */
 
 // Structure to represent a point in 2D plane
 struct Point {
@@ -99,68 +182,6 @@ double closestPair(const vector<Point>& points) {
 }
 
 
-using namespace std;
-
-struct bucket{
-    int id;
-    bool full;
-
-};
-
-// requires the array to be sorted
-int storage(bucket arr[],int start, int end){
-    if(arr[end].full == 0){
-        return 0;
-    }
-    if(arr[start].full == 1){
-        return (end+1-start);
-
-    } else if (arr[start].full == 0){
-            return storage(arr, start, start+(end-start)/2) + storage(arr, (start+(end-start)/2)+1, end);} else {
-
-
-
-    }
-
-
-
-}
-
-int maxhelper(int arr[], int s, int e) {
-    // initialization takes constant time
-    int sum = 0;
-    int left = INT_MIN;
-    int right = INT_MIN; // Fix: Initialize right to INT_MIN instead of INT_MAX
-    int middle = (s + e) / 2;
-
-    // each for loop takes n/2 so n/2 + n/2 = n so O(n)
-    for (int i = middle + 1; i <= e; i++) { // Fix: Change the loop condition to <= instead of >=
-        sum += arr[i];
-        right = std::max(right, sum); // Fix: Change left to right
-    }
-
-    sum = 0; // Reset sum for the next loop
-
-    for (int i = middle; i >= s; i--) {
-        sum += arr[i];
-        left = std::max(left, sum); // Fix: Change right to left
-    }
-
-    return left + right;
-}
-
-int max(int arr[], int s, int e) {
-    if (s == e) {
-        return arr[s];
-    }
-
-    int middle = (s + e) / 2;
-    int maxleft = max(arr, s, middle);
-    int maxright = max(arr, middle + 1, e);
-    int maxoverlap = maxhelper(arr, s, e);
-
-    return std::max({maxleft, maxright, maxoverlap}); // Fix: Use std::max to find the maximum of three values
-}
 
 
 
@@ -168,6 +189,9 @@ int max(int arr[], int s, int e) {
 
 
 int main(){
+
+    // Part 1
+
     bucket arr[] = {{11,1},
                     {12,0},
                     {13,0},
@@ -183,13 +207,16 @@ int main(){
     cout << endl;
 
 
-
+    // Part 2
 
     int arr2[10] = {100,90,-80,60,40,-100,30,20,-10,0};
     cout << max(arr2, 0, 9);
 
 
     cout << endl;
+
+
+    //Part 3
 
     // Example usage
     vector<Point> points1 = {{2, 3}, {12, 30}, {40, 50}, {5, 1}, {12, 10}, {3, 4}};  //min dist bet. (2,3),(3,4)
