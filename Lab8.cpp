ce8726517bd2c8cb0f74ef83086b73c62cc3e7d8
MarwanMo11 <MarwanMo11@aucegypt.edu>
2024-03-22 15:40:41 +0200

    added comments and complexity and organized code

--- Lab8.cpp
+++ Lab8.cpp
@@ -8,14 +8,16 @@ using namespace std;
 
 int comb(int target, vector<int> arr){
     int DP[arr.size()+1][target+1];
+    // Filling in the base case
     for(int i = 0; i < arr.size()+1; i++){
         DP[i][0] = 1;
     }
-
+    // Filling in the base case
     for (int i = 1; i < target+1; i++){
         DP[0][i] = 0;
     }
 
+    // Calculating remaining cells 
     for (int i = 1; i < arr.size()+1; i++){
         for (int j = 1; j <= target; j++){
             if(arr[i-1] > j){
---
+++ lab7.cpp
@@ -0,0 +1,77 @@
+//
+// Created by Marwan on 3/22/2024
+//
+
+
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+vector<vector<int>> get_optimal_split(const vector<int>& dims) {
+    int n = dims.size() - 1; // Number of matrices
+    vector<vector<int>> dp(n, vector<int>(n, 0)); // Dynamic programming table for memoization
+    vector<vector<int>> split(n, vector<int>(n, 0)); // Table to store the optimal split
+
+    for (int len = 2; len <= n; len++) {
+        for (int i = 0; i < n - len + 1; i++) {
+            int j = i + len - 1;
+            dp[i][j] = INT_MAX; // Initialize to maximum value
+
+            for (int k = i; k < j; k++) {
+                int cost = dp[i][k] + dp[k + 1][j] + dims[i] * dims[k + 1] * dims[j + 1];
+                if (cost < dp[i][j]) {
+                    dp[i][j] = cost;
+                    split[i][j] = k;
+                }
+            }
+        }
+    }
+    return split;
+}
+
+void print_parenthesis(const vector<vector<int>>& split, int i, int j) {
+    if (i == j) {
+        cout << "A" << i + 1;
+    } else {
+        cout << "(";
+        print_parenthesis(split, i, split[i][j]);
+        print_parenthesis(split, split[i][j] + 1, j);
+        cout << ")";
+    }
+}
+
+void print_optimal(const vector<int>& dims) {
+    vector<vector<int>> split = get_optimal_split(dims);
+    int n = dims.size() - 1;
+
+    cout << "Optimal Parenthesization: ";
+    print_parenthesis(split, 0, n - 1);
+    cout << endl;
+}
+
+
+
+
+
+
+int main(){
+    vector<int> dims = {30, 35, 15, 5, 10, 20, 25}; // Test case mentioned
+    cout << "Test case: ";
+    for (int dim : dims) {
+        cout << dim << " ";
+    }
+    cout << endl;
+
+    print_optimal(dims);
+
+    vector<int> dims2 = {4, 4, 6, 12, 7}; // Additional test case
+    cout << "Additional test case: ";
+    for (int dim : dims2) {
+        cout << dim << " ";
+    }
+    cout << endl;
+
+    print_optimal(dims2);
+    return 0;
+}
