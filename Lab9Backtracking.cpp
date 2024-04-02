ed8ea685d10c908b60e7b43a4f7717576788678d
MarwanMo11 <MarwanMo11@aucegypt.edu>
2024-04-02 20:54:32 +0200

    Solved Problem two in Lab9

--- Lab9Backtracking.cpp
+++ Lab9Backtracking.cpp
@@ -31,10 +31,62 @@ void CodeCrackerCaller(int n, int range, int& counter){
 
 }
 
+void ID(vector<int> IDs, int x, int teamsize, vector<int> result, int junior, int senior, vector<bool>& used, int& counter, int start) {
+    if (teamsize == 3 && senior > 0) {
+        for (int i = 0; i < result.size(); i++) {
+            cout << result[i] << " ";
+        }
+        cout << endl;
+        counter++;
+        return;
+    }
 
-int main(){
-    int counter = 0;
-    CodeCrackerCaller(4, 6, counter);
-    cout << "Total number of codes: " << counter << endl;
+    for (int i = start; i < IDs.size(); i++) {
+        if (used[i]) continue; // Skip if this ID has already been used
+        result.push_back(IDs[i]);
+        used[i] = true; // Mark this ID as used
+        if (IDs[i] > x) {
+            junior++;
+        } else {
+            senior++;
+        }
+        ID(IDs, x, teamsize + 1, result, junior, senior, used, counter, i + 1);
+        used[i] = false; // Unmark this ID as used
+        result.pop_back();
+        if (IDs[i] > x) {
+            junior--;
+        } else {
+            senior--;
+        }
+    }
+}
+
+
+int main() {
+    // List of employee IDs
+    vector<int> IDs = {10, 15, 20, 25, 30};
+    // Threshold for seniority
+    int X = 25;
+    // Team size
+    int teamSize = 0;
+    // Junior and senior counts
+    int junior = 0, senior = 0;
+    // Vector to hold the current team
+    vector<int> result;
+    // Vector to keep track of which IDs have been used
+    vector<bool> used(IDs.size(), false);
+
+    int counter1 = 0;
+
+    // Call the ID function to generate the teams
+    ID(IDs, X, teamSize, result, junior, senior, used,counter1,0);
+
+    cout << "Total number of teams: " << counter1 << endl << endl;
+
+    int counter2 = 0;
+    CodeCrackerCaller(4, 6, counter2);
+    cout << "Total number of codes: " << counter2 << endl;
+
+
     return 0;
 }
